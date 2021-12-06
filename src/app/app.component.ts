dc6a561e817624ad908f171e43674c214c8922e7
wizardnet972 <wizardnet972@gmail.com>
2021-12-06 12:14:36 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <form [formGroup]="form">
+      <formly-form
+        [model]="model"
+        [fields]="fields"
+        [options]="options"
+        [form]="form"
+      ></formly-form>
+    </form>
+    <button (click)="updateModel()">update model<button></button></button>
+  `,
 })
 export class AppComponent {
-  title = 'ng-formly';
+  form = new FormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'foo',
+      type: 'custom-field',
+    },
+  ];
+
+  updateModel() {
+    this.model = {
+      foo: [1, 2, 3],
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,69 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component, ChangeDetectionStrategy } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  AbstractControl,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+
+import { FieldType } from '@ngx-formly/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  template: `im MyCustomComponent!! formControl.value:
+    {{ formControl.value | json }} `,
+})
+export class MyCustomComponent extends FieldType {
+  ngOnInit() {
+    console.log('in ngOnInit');
+
+    this.formControl.valueChanges.subscribe((v) => {
+      console.log({ v });
+    });
+  }
+
+  ngOnDestroy() {
+    console.log('in ngOnDestroy');
+  }
+
+  constructor() {
+    super();
+  }
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [MyCustomComponent],
   imports: [
+    CommonModule,
+    FormlyModule.forChild({
+      extras: { lazyRender: true },
+      types: [{ name: 'custom-field', component: MyCustomComponent }],
+    }),
+  ],
+})
+export class CustomFieldModule {}
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomFieldModule,
+    FormlyModule.forRoot({
+      validationMessages: [
+        { name: 'required', message: 'This field is required' },
+      ],
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
